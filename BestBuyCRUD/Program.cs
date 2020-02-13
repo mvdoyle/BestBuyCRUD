c0485f488def69f97f96dc9c15a34a2341ce5f24
mvdoyle <mdoyle@truecoders.io>
2020-02-13 09:28:51 -0600

    added gitignore

---
+++ BestBuyCRUD/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace BestBuyCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var departments = GetAllDepartments(); //a list of departments from the database
+
+            foreach (var department in departments) // for each item in our list of departments, print the department item
+            {
+                Console.WriteLine(department);
+
+            }
+        }
+
+        static List<string> GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+
+           
+                conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+           
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT Name FROM Departments;";
+
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read() == true)
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+
+                return allDepartments;
+            }
+        }
+    }
+
+}
